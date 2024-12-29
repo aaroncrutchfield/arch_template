44ade13492666fe589d68bf0843636b2eede34a0
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2024-12-28 23:15:06 -0500

    feat: Initialize auth_repository package with basic structure and tests (#5)
    - Created the auth_repository package with essential files including pubspec.yaml, README.md, and .gitignore.
    - Implemented the AuthRepository class with a basic instantiation test.
    - Added Flutter widget tests for the main App to ensure the CounterPage renders correctly.
    - Included a coverage badge SVG for visual representation of test coverage.
    - Configured analysis options for code quality checks.
    
    AT-2

---
+++ packages/auth_repository/lib/src/auth_repository.dart
@@ -0,0 +1,7 @@
+/// {@template auth_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class AuthRepository {
+  /// {@macro auth_repository}
+  const AuthRepository();
+}
