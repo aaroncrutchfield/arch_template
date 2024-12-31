d85d4aa1471a539557e7c9461a2442d4b1aae2a9
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2024-12-30 19:59:49 -0500

    Revert "feat: Platform login with Apple and Google (#9)"
    This reverts commit 9ff8fbfe5ce33945b671ad9982829c2819f6926a.

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,40 +1,14 @@
-import 'package:arch_template/core/di/app_registry.dart';
-import 'package:arch_template/core/navigation/navigation.dart';
-import 'package:arch_template/features/auth/bloc/auth_bloc.dart';
+import 'package:arch_template/counter/counter.dart';
 import 'package:arch_template/l10n/l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider.value(
-      value: appRegistry.get<AppNavigation>(),
-      child: BlocProvider.value(
-        value: appRegistry.get<AuthBloc>(),
-        child: const AppView(),
-      ),
-    );
-  }
-}
-
-class AppView extends StatelessWidget {
-  const AppView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final navigation = context.read<AppNavigation>();
-    final colorScheme = ColorScheme.fromSeed(
-      brightness: MediaQuery.platformBrightnessOf(context),
-      seedColor: Colors.orange,
-    );
-
-    return MaterialApp.router(
-      routerConfig: navigation.routerConfig(),
+    return MaterialApp(
       theme: ThemeData(
-        colorScheme: colorScheme,
         appBarTheme: AppBarTheme(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
@@ -42,6 +16,7 @@ class AppView extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+      home: const CounterPage(),
     );
   }
 }
--- packages/auth_repository/lib/auth_repository.dart
+++
@@ -1,5 +0,0 @@
-/// A repository for authentication.
-library;
-
-export 'src/data/models/models.dart';
-export 'src/domain/auth_repository.dart';
