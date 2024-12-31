ab9151e416f5610d004fde2562d0e76887a5c532
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2024-12-30 20:26:41 -0500

    feat: Platform login with Apple and Google (#11)
    * feat: Add AutoRoute for navigation and update dependencies(#4)
    
    * feat: Initialize auth_repository package with basic structure and tests (#5)
    
    * Implement Firebase AuthRepository (#6)
    
    * Create LoginPage and integrated it with the authentication flow(#7)
    
    * fix: Navigation does not occur after successful login(#8)

---
+++ lib/features/login/bloc/login_state.dart
@@ -0,0 +1,23 @@
+part of 'login_bloc.dart';
+
+sealed class LoginState extends Equatable {
+  const LoginState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginLoading extends LoginState {}
+
+final class LoginSuccess extends LoginState {}
+
+final class LoginFailure extends LoginState {
+  const LoginFailure(this.message);
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
