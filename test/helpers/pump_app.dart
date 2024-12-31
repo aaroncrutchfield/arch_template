ea8627c7cf7123d8a66d94518498c42244660afa
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2024-12-31 02:24:01 -0500

    dart fix

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,7 +1,5 @@
 import 'package:arch_template/l10n/arb/app_localizations.dart';
-import 'package:arch_template/l10n/arb/app_localizations_en.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension PumpApp on WidgetTester {
